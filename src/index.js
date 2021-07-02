459591740541b9731f76aa6c0b43078efc9a3198
Danil Larin <PickleDan@yandex.ru>
2021-07-02 14:46:20 +0300

    css, scss, postcss added to project

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import _ from 'lodash';
+import './styles/index.scss'
+
 
 function component() {
   const element = document.createElement('div');
 
-
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
   return element;
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,48 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+let mode = 'development'
+
+if (process.env.NODE_ENV === 'production') {
+  mode = "production"
+}
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+  mode,
+
+  module: {
+    rules: [
+      {
+        test: /\.s?css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader", "sass-loader"]
+      },
+
+      {
+        test: /\.js$/,
+        exclude: /nide_modules/,
+        use: {
+          "loader": "babel-loader"
+        },
+      },
+    ]
   },
-  mode: "development"
+
+  entry: './src/index.js',
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
+    hot: true
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Development',
+    }),
+    new MiniCssExtractPlugin()
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
 };
